12ef95f0bbfe59e1024c0788a23927323304183c
JosePonferrada <joseponfe10@gmail.com>
2025-05-09 21:21:16 +0200

    feat: add BarChart and LineChart components for data visualization

---
+++ formula1-app/resources/js/components/Charts/LineChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <Line
+    :data="chartData"
+    :options="options"
+  />
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale)
+
+export default {
+  name: 'LineChart',
+  components: { Line },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
