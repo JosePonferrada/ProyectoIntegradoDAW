8ee4b1922a9571a4b66a37b5dc7b6377167f9c54
JosePonferrada <joseponfe10@gmail.com>
2025-05-29 08:23:19 +0200

    feat: add date-fns-tz, vue-draggable-next, and vue-the-mask dependencies; integrate VueTheMask in app setup

--- formula1-app/resources/js/app.ts
+++ formula1-app/resources/js/app.ts
@@ -7,6 +7,7 @@ import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import VueTheMask from 'vue-the-mask';
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
@@ -30,6 +31,7 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(VueTheMask)
             .mount(el);
     },
     progress: {
