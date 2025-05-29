3d24bad98fa92944c37ff1c6614b2fbcd503dc64
JosePonferrada <joseponfe10@gmail.com>
2025-05-29 08:30:49 +0200

    feat: Add scheduled command to update race status every minute; log output to race-status-update.log

--- formula1-app/routes/console.php
+++ formula1-app/routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\UpdateRaceStatusCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('race:update-status-from-fp1')
+         ->everyMinute()
+         ->appendOutputTo(storage_path('logs/race-status-update.log'));
